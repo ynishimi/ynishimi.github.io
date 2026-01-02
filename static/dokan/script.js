6ace39b3fc7357f2f45ca94d8e7215fe6faaa2c3
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2026-01-02 22:42:31 +0100

    finished poc

--- static/dokan/script.js
+++ static/dokan/script.js
@@ -2,7 +2,10 @@ var peer = new Peer();
 
 peer.on('open', function(id) {
 	console.log('My peer ID is: ' + id);
-  });
+    const para = document.createElement("p");
+    para.innerText = ('your ID is: ' + id);
+    document.body.appendChild(para);
+});
 
 // get dest peer ID
 const btn = document.getElementById('connect-btn');
@@ -22,22 +25,52 @@ peer.on('connection', function(conn) {
 });
 
 function sendrecv(conn) {
-    console.log("connected");
-
     conn.on('open', function() {
+        console.log("connected", conn.peer);
+        const statusPara = document.createElement("p");
+        statusPara.innerText = ('connected with a peer: ' + conn.peer);
+        document.body.appendChild(statusPara);
+    });
+
         // input file
         const input = document.getElementById('file-selector');
         input.onchange = e => { 
             // getting a hold of the file reference
             const file = e.target.files[0]; 
-            conn.send(file);
-            console.log("sent", file);
+            if (conn.open) {
+                conn.send({
+                    filename: file.name,
+                    body: file,
+                });
+                console.log("sent", file);
+            }
+            else {
+                console.log("conn closed");
+                const closedPara = document.createElement("p");
+                closedPara.innerText = ('connection with a peer closed: ' + conn.peer);
+                document.body.appendChild(closedPara);
+            }
         }
-    });
 
     // Receive messages
     conn.on('data', function(data) {
         // receive data
-        console.log('Received', data);
+        console.log('received', data);
+        const receivedPara = document.createElement("p");
+        receivedPara.innerText = ('downloaded ' + data.filename);
+        document.body.appendChild(receivedPara);
+
+        
+        const blob = new Blob([data.body]);
+
+        // download data
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = data.filename;
+        a.click();
+    });
+    conn.on('close', function() {
+        console.log('closed');
     });
 }
